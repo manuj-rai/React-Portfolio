5a7fdaed140878739e7aa557c0dc65935838c483
Manuj Rai <imanujrai7@gmail.com>
2025-01-15 14:59:06 +0530

    modified:   components/ProjectsBtn.jsx

--- components/ProjectsBtn.jsx
+++ components/ProjectsBtn.jsx
@@ -8,7 +8,8 @@ const ProjectsBtn = () => {
     <div className="mx-auto xl:mx-0">
       <Link
         href="/work"
-        className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group"
+        style={{ zIndex: 999 }}
+        className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group z-[999]"
       >
         <Image
           src="/rounded-text.png"
