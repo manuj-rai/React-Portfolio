2e821af4c1277c7f6e1ef3caa64344b8b21e5f3a
Manuj Rai <imanujrai7@gmail.com>
2025-01-23 12:07:16 +0530

    modified:   components/Transition.jsx

--- components/Transition.jsx
+++ components/Transition.jsx
@@ -25,7 +25,7 @@ const Transition = () => {
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0.1, duration: 0.3, ease: "easeInOut" }}
+        transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
         aria-hidden
       />
       <motion.div
@@ -35,7 +35,7 @@ const Transition = () => {
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0.2, duration: 0.3, ease: "easeInOut" }}
+        transition={{ delay: 0.4, duration: 0.6, ease: "easeInOut" }}
         aria-hidden
       />
       <motion.div
@@ -45,7 +45,7 @@ const Transition = () => {
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0.3, duration: 0.3, ease: "easeInOut" }}
+        transition={{ delay: 0.6, duration: 0.6, ease: "easeInOut" }}
         aria-hidden
       />
     </>
